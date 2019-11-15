1b171b5f0cd2dee932af3197e49ce7bfcc4fcee0
Cole <coleb29@academic.uhd.campus>
2019-11-14 19:25:31 -0600

    avgGrade update

---
+++ query/avgGrade.js
@@ -0,0 +1,15 @@
+module.exports = {
+    avgGrade: (req, res) => {
+        db.query("SELECT AVG(grade) FROM grade where enrolledID="+req.params.ID, (err, result) => {
+            if (err) {
+                res.redirect("/");
+                console.log(err);
+            }
+        res.render('addGrade.ejs', {
+                title: 'Welcome to LearnHub | Add a grade',
+                assns: result,
+		ID: req.params.ID
+            });
+        });
+    },
+};
--- query/viewGrade.js
+++ query/viewGrade.js
@@ -1,13 +1,14 @@
 module.exports = {
     viewGrade: (req, res) => {
-        db.query(`select AVG(grade) 'avg', u.name,g.grade, g.gradeID from grade g, enrolled e, user u group by g.grade where u.userID=e.userID and g.enrolledID=e.enrollmentID and e.enrollmentID=?;`,req.params.ID, (err, result) => {
+        db.query(`"select u.name,g.grade, g.gradeID from grade g, enrolled e, user u where u.userID=e.userID and g.enrolledID=e.enrollmentID and e.enrollmentID="+req.params.ID+";select AVG(g.grade) 'avg' from grade g, enrolled e group by g.enrolledID="+req.params.ID, (err, result) => {
             if (err) {
                 res.redirect("/");
                 console.log(err);
             }
         res.render('viewUserGrade.ejs', {
                 title: 'Welcome to LearnHub |  Grades',
-                grades: result,
+                grades: result[0],
+                avg: result[0]
             });
         });
     },
